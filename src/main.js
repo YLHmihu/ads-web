8767c65b054d8b00ed4eea3468389a0c430a39a4
YLHmihu <1272445005@qq.com>
2021-06-22 09:57:11 +0800

    添加

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "lib-flexible";//全局px转rem适配
 
 createApp(App).use(store).use(router).mount("#app");
